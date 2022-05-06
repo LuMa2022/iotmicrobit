c126965624fdc360ade1ddd838eb18fb477955df
LuMa2022 <104971308+LuMa2022@users.noreply.github.com>
2022-05-06 08:25:17 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,6 @@
-basic.showIcon(IconNames.Heart)
-I2C_LCD1602.LcdInit(39)
-I2C_LCD1602.ShowString("Hello", 3, 0)
-I2C_LCD1602.BacklightOn()
-let value = 0
+pins.servoWritePin(AnalogPin.P9, 0)
+basic.pause(100)
 basic.forever(function () {
-    value += 1
-    I2C_LCD1602.ShowNumber(value, 7, 1)
+    pins.servoWritePin(AnalogPin.P9, 0)
     basic.pause(500)
 })
